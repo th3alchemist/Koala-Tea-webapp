48efed95e45a3758fe805f2450c84c4a9b3c3db6
TJJankowski <tyler.jankowski@hotmail.com>
2019-10-07 22:07:57 -0500

    added login service functionality and injected it into the login page

--- src/app/services/loginService/login.service.ts
+++ src/app/services/loginService/login.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  submitLoginForm(email:string, password:string) {
+    let headers = new HttpHeaders().set("Content-Type", "application/x-www-form-urlencoded");
+
+    let body = new HttpParams().set("email", email).set("password", password);
+
+    return this.http.post("http://localost:8090/user/login", body, {headers: headers});
+  }
+
 }
