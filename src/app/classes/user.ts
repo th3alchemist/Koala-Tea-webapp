b33ce6b6a129b57e8bb2afb9bda1d9cb650dfa0e
joannacm <joannamagdaraog@gmail.com>
2019-10-09 12:02:44 -0500

    pushing create recipe, added getter and setters

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,133 @@
 export class User {
+
+    private id:number;
+    private email:string;
+    private password:string;
+    private fname:string;
+    private lname:string;
+    private address:string;
+    private dateOfBirth:string;
+
+	constructor($id: number, $email: string, $password: string, $fname: string, $lname: string, $address: string, $dateOfBirth: string) {
+		this.id = $id;
+		this.email = $email;
+		this.password = $password;
+		this.fname = $fname;
+		this.lname = $lname;
+		this.address = $address;
+		this.dateOfBirth = $dateOfBirth;
+	}
+    
+
+    /**
+     * Getter $id
+     * @return {number}
+     */
+	public get $id(): number {
+		return this.id;
+	}
+
+    /**
+     * Getter $email
+     * @return {string}
+     */
+	public get $email(): string {
+		return this.email;
+	}
+
+    /**
+     * Getter $password
+     * @return {string}
+     */
+	public get $password(): string {
+		return this.password;
+	}
+
+    /**
+     * Getter $fname
+     * @return {string}
+     */
+	public get $fname(): string {
+		return this.fname;
+	}
+
+    /**
+     * Getter $lname
+     * @return {string}
+     */
+	public get $lname(): string {
+		return this.lname;
+	}
+
+    /**
+     * Getter $address
+     * @return {string}
+     */
+	public get $address(): string {
+		return this.address;
+	}
+
+    /**
+     * Getter $dateOfBirth
+     * @return {string}
+     */
+	public get $dateOfBirth(): string {
+		return this.dateOfBirth;
+	}
+
+    /**
+     * Setter $id
+     * @param {number} value
+     */
+	public set $id(value: number) {
+		this.id = value;
+	}
+
+    /**
+     * Setter $email
+     * @param {string} value
+     */
+	public set $email(value: string) {
+		this.email = value;
+	}
+
+    /**
+     * Setter $password
+     * @param {string} value
+     */
+	public set $password(value: string) {
+		this.password = value;
+	}
+
+    /**
+     * Setter $fname
+     * @param {string} value
+     */
+	public set $fname(value: string) {
+		this.fname = value;
+	}
+
+    /**
+     * Setter $lname
+     * @param {string} value
+     */
+	public set $lname(value: string) {
+		this.lname = value;
+	}
+
+    /**
+     * Setter $address
+     * @param {string} value
+     */
+	public set $address(value: string) {
+		this.address = value;
+	}
+
+    /**
+     * Setter $dateOfBirth
+     * @param {string} value
+     */
+	public set $dateOfBirth(value: string) {
+		this.dateOfBirth = value;
+	}
 }
--- src/app/components/create-recipe/create-recipe.component.ts
+++ src/app/components/create-recipe/create-recipe.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { Unit } from 'src/app/classes/unit';
+import { UnitService } from 'src/app/services/unitService/unit.service';
+import { Recipe } from 'src/app/classes/recipe';
+import { Ingredient } from 'src/app/classes/ingredient';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Cookbook } from 'src/app/classes/cookbook';
+import { User } from 'src/app/classes/user';
+import { RecipeService } from 'src/app/services/recipeService/recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-recipe',
@@ -7,15 +16,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRecipeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us:UnitService, private rs:RecipeService,
+              private router:Router) { }
 
   ngOnInit() {
+    this.getAllUnits();
   }
 
-  units: String[] = ["mL", "cup", "teaspoon", "tablespoon"];
+  data:any;
 
- 
+  units: Unit[] = [];
 
+  getAllUnits(){
+    this.us.getAllUnits().subscribe(
+      data => {
+        this.units = data;
+        console.log(this.units);
+      },
+      error => {
+        error = "Sorry. Couldn't get those units!"
+        console.log(error);
+      }
+
+    )
+  }
+
+  // cooklist : Array<Cookbook> = [];
+  cookbook:Number[] = [1,2];
+  // //CHECKBOX FOR PUBLIC
+  // isChecked:boolean = false;
+
+  // check(){
+  //   this.isChecked = !this.isChecked;
+  // }
+
+counter:number = 1;
 
   addAnother(){
     let field = document.getElementById('ingredient_block');
@@ -29,7 +64,7 @@ export class CreateRecipeComponent implements OnInit {
     label1.innerHTML = "Ingredient";
     let input1 = document.createElement('input');
     input1.setAttribute("type", "text");
-    input1.setAttribute("class", "form-control");
+    input1.setAttribute("class", "ingredient form-control");
     input1.setAttribute("id", "ingredients");
     input1.setAttribute("placeholder", "Ingredients");
     input1.setAttribute("name", "ingredient");
@@ -43,10 +78,10 @@ export class CreateRecipeComponent implements OnInit {
     label2.innerHTML = "Amount";
     let input2 = document.createElement('input');
     input2.setAttribute("type", "text");
-    input2.setAttribute("class", "form-control");
+    input2.setAttribute("class", "amount form-control");
     input2.setAttribute("id", "amount");
     input2.setAttribute("placeholder", "Amount");
-    input1.setAttribute("name", "amount");
+    input2.setAttribute("name", "amount");
     div2.appendChild(label2);
     div2.appendChild(input2);
     //UNIT INPUT
@@ -58,11 +93,11 @@ export class CreateRecipeComponent implements OnInit {
     let input3 = document.createElement('select');
     input3.setAttribute("id", "unit");
     input3.setAttribute("name", "unit");
-    input3.setAttribute("class", "form-control");
+    input3.setAttribute("class", "unit form-control");
     for(var i = 0; i < this.units.length; i++){
       let option = document.createElement('option');
-      option.value = i.toString();
-      option.innerText = this.units[i].toString();
+      option.value = this.units[i].$id.toString();
+      option.innerText = this.units[i].$unit;
       input3.appendChild(option);
     }
     div3.appendChild(label3);
@@ -73,7 +108,52 @@ export class CreateRecipeComponent implements OnInit {
     div.appendChild(div3);
 
     field.appendChild(div);
+    this.counter += 1;
+    console.log(this.counter);
+  }
+  listOfIngredientName : string [] = [];
+  listOfIngredientAmount : number [] = [];
+  listOfIngredientUnit : number [] = [];
+  
 
+  submitRecipe(){
+    let cookbookid = Number((<HTMLInputElement>document.getElementById('cookbook')).value)
+    let title = (<HTMLInputElement>document.getElementById('title')).value
+    let shared = Boolean((<HTMLInputElement>document.getElementById('public_check')).value)
+    let instruction = (<HTMLInputElement>document.getElementById('instructions')).value
+    let ingredients = (<HTMLCollection>document.getElementsByClassName('formbox'))[0];
+    
+    for(var i=0; i<this.counter; i++){
+      this.listOfIngredientName.push((<HTMLInputElement>ingredients.getElementsByClassName("ingredient")[i]).value);
+      this.listOfIngredientAmount.push(Number((<HTMLInputElement>ingredients.getElementsByClassName("amount")[i]).value));
+      this.listOfIngredientUnit.push(Number((<HTMLInputElement>ingredients.getElementsByClassName("unit")[i]).value))
+    }
+   
+    //THIS SUBMITS RECIPE OBJECT ONLY
+    this.rs.submitRecipe(cookbookid, title, shared, instruction).subscribe(
+      data => {
+          this.data = data;
+          console.log(this.data);
+          //AFTER SUBMITTING RECIPE, RECIPE OBJECT IS RETURNED. THEN SUBMIT INGREDIENT WHICH NEEDS THE RECIPE OBJECT
+          this.rs.submitIngredient(this.data, this.listOfIngredientName, this.listOfIngredientAmount, this.listOfIngredientUnit).subscribe(
+            data => {
+              this.data = data;
+              console.log(this.data);
+              if(this.data !== null){
+                this.router.navigate(['/cookbook']);
+              }
+            },
+            error => {
+              error = "INGREDIENT INSERT FAIL.";
+          console.log(error)
+            }
+          )
+      },
+      error => {
+        error = "RECIPE INSERT FAIL.";
+        console.log(error)
+      }
+    );
   }
   
 }
