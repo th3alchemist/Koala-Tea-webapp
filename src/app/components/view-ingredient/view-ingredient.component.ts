5d9f7f6862f0da6ee6ae5f3ef25a95be2b774de0
th3alchemist_0 <ayo.j.arum@gmail.com>
2019-10-10 13:44:12 -0500

    added recipe search feature

--- src/app/components/view-ingredient/view-ingredient.component.ts
+++ src/app/components/view-ingredient/view-ingredient.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cookbook } from 'src/app/classes/cookbook';
+import { SpoonService } from 'src/app/services/spoon/spoon.service'
 
 @Component({
   selector: 'app-view-ingredient',
@@ -7,14 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewIngredientComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ss:SpoonService) { }
 
   ngOnInit() {
   }
 
-  id:number;
-  name:string;
-  amount:number;
-  recipe:string;
-  unit:string;
+  data:any
+
+  onSearch(){
+    var query_ele = document.getElementById("query");
+    var query = (<HTMLInputElement> query_ele).value
+//HTMLSelectElement
+    var cuisine_ele = <HTMLSelectElement>document.getElementById("cuisine");
+    var cuisine = cuisine_ele.options[cuisine_ele.selectedIndex].value
+
+    var course_ele = <HTMLSelectElement>document.getElementById("course");
+    var course = course_ele.options[course_ele.selectedIndex].value
+
+    var diet_ele = <HTMLSelectElement>document.getElementById("diet");
+    var diet = diet_ele.options[diet_ele.selectedIndex].value
+
+    var intolerances_ele = <HTMLSelectElement>document.getElementById("intolerances");
+    var intolerance = intolerances_ele.options[intolerances_ele.selectedIndex].value
+
+    this.ss.searchRecipe(query, course, cuisine, diet, intolerance).subscribe(
+      data => {
+        this.data = data
+        console.log(this.data.title);
+
+        var title_ele = <HTMLInputElement>document.getElementById('title')
+
+        title_ele.value = this.data.title
+      })
+  }
 }
