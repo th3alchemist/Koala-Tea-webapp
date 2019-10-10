0454db32a659213795878a9a8fc8d68091a2b521
th3alchemist_0 <ayo.j.arum@gmail.com>
2019-10-10 15:56:01 -0500

    added recipe endpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { CreateUserAccountComponent } from './components/create-user-account/cre
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ViewIngredientComponent } from './components/view-ingredient/view-ingredient.component';
 import { EditCookbookComponent } from './components/edit-cookbook/edit-cookbook.component';
+import { SearchRecipeComponent } from './components/search-recipe/search-recipe.component';
 
 const routes: Routes = [
   {
@@ -41,6 +42,10 @@ const routes: Routes = [
   {
     path: "editCookbook",
     component: EditCookbookComponent
+  },
+  {
+    path: "searchRecipe",
+    component: SearchRecipeComponent
   }
 ];
 
